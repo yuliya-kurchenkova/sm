60e42a0f7ed523bb1131fc096276e3dc2f6dc610
yuliya-kurchenkova <iulia.kurchenkova@yandex.by>
2024-11-28 14:14:04 +0300

    layout profile card component

---
+++ src/app/common-ui/profile-card/profile-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-card',
+  imports: [],
+  templateUrl: './profile-card.component.html',
+  styleUrl: './profile-card.component.scss'
+})
+export class ProfileCardComponent {}
