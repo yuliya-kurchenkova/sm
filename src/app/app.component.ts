e8a5be332645ed6f423886ab48073963815f5146
yuliya-kurchenkova <iulia.kurchenkova@yandex.by>
2024-11-29 10:38:42 +0300

    add service, parsed the request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileCardComponent } from "./common-ui/profile-card/profile-card.component";
+import { ProfileService } from './shared/services/profile/profile.service';
+import { Test_Account } from './shared/interfaces/account.interface';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,12 @@ import { ProfileCardComponent } from "./common-ui/profile-card/profile-card.comp
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  profileService = inject(ProfileService);
+  profiles: Test_Account[] = []
+
+  constructor() {
+    this.profileService.getTestAccounts().subscribe(
+      data => this.profiles = data
+    )
+  }
 }
