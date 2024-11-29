786d20c0c5ea7f12c0e270201044d196bf9150cd
yuliya-kurchenkova <iulia.kurchenkova@yandex.by>
2024-11-29 15:14:52 +0300

    add pages search, login, profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProfileCardComponent } from "./common-ui/profile-card/profile-card.component";
-import { ProfileService } from './shared/services/profile/profile.service';
-import { Test_Account } from './shared/interfaces/account.interface';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfileCardComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  profileService = inject(ProfileService);
-  profiles: Test_Account[] = []
-
-  constructor() {
-    this.profileService.getTestAccounts().subscribe(
-      data => this.profiles = data
-    )
-  }
-}
+export class AppComponent {}
---
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-page',
+  imports: [],
+  templateUrl: './profile-page.component.html',
+  styleUrl: './profile-page.component.scss'
+})
+export class ProfilePageComponent {
+
+}
