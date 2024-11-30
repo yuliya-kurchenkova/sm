8915156034a0240878c30801dce192dceeb74275
yuliya-kurchenkova <iulia.kurchenkova@yandex.by>
2024-11-30 12:45:59 +0300

    add standard behavior formGroup

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+  form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl('')
+  })
 
+
+  onSubmit(event: Event) {
+    console.log(this.form.value); 
+    // {
+    //     "username": "YliyaKurcha",
+    //     "password": "dovCQq8d74"
+    // }
+  }
 }
