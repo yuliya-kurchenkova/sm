101c8e3dff11806f186a00d3ac0a18fc40f46e19
yuliya-kurchenkova <iulia.kurchenkova@yandex.by>
2024-11-30 18:01:00 +0300

    add variables token and refreshToken for check authorization user

--- src/app/shared/auth/auth-service/auth.service.ts
+++ src/app/shared/auth/auth-service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { LoginBody } from '../../interfaces/auth.interface';
+import { LoginBody, TokenResponse } from '../../interfaces/auth.interface';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,21 @@ export class AuthService {
   http = inject(HttpClient);
   baseUrl = 'https://icherniakov.ru/yt-course/auth';
 
+  token: string | null = null;
+  refreshToken: string | null = null;
+
   login(body: LoginBody) {
     const fd = new FormData();
     fd.append('username', body.username);
     fd.append('password', body.password);
-    return this.http.post(`${this.baseUrl}/token`, fd)
+
+    return this.http.post<TokenResponse>(`${this.baseUrl}/token`, fd)
+    .pipe(
+      tap(val => {
+        console.log(val); // {access_token: "token", refresh_token: "token", token_type: "Bearer"}
+        this.token = val.access_token;
+        this.refreshToken = val.refresh_token;
+      })
+    )
   }
 }
--- src/app/shared/interfaces/auth.interface.ts
+++ src/app/shared/interfaces/auth.interface.ts
@@ -2,3 +2,9 @@ export interface LoginBody {
   username: string,
   password: string
 }
+
+export interface TokenResponse {
+  access_token: string,
+  refresh_token: string,
+  token_type: string
+}
