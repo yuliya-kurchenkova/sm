cadedd5dd3f26b3632fc98e2b40c5fe0e0ea5e91
yuliya-kurchenkova <iulia.kurchenkova@yandex.by>
2024-11-30 19:19:44 +0300

    add cookie service and add token and refreshToken into cookie

--- src/app/shared/auth/auth-service/auth.service.ts
+++ src/app/shared/auth/auth-service/auth.service.ts
@@ -2,18 +2,24 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { LoginBody, TokenResponse } from '../../interfaces/auth.interface';
 import { tap } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   http = inject(HttpClient);
+  cookieService = inject(CookieService);
   baseUrl = 'https://icherniakov.ru/yt-course/auth';
 
   token: string | null = null;
   refreshToken: string | null = null;
 
   get isAuth() {
+    if (!this.token) {
+      this.token = this.cookieService.get('token'); // Получение токена из куков на случай перезагрузки страницы
+    }
+
     return !!this.token;  // Возвращает true, если токен существует
   }
 
@@ -28,6 +34,9 @@ export class AuthService {
         console.log(val); // {access_token: "token", refresh_token: "token", token_type: "Bearer"}
         this.token = val.access_token;
         this.refreshToken = val.refresh_token;
+
+        this.cookieService.set('token', this.token);
+        this.cookieService.set('refreshToken', this.refreshToken);
       })
     )
   }
